5b0a0c4bbc195b61ef17bbcf0eaf566251b75922
Lina <linazon94@gmail.comgit>
2021-05-23 20:00:56 -0500

    se agrega tenario para reemplazar a 2 if, pero codemetric indica que esto es demasiado complejo así que se deja la primera opcion

--- Ejercicio_1/untitled/src/com/company/Main.java
+++ Ejercicio_1/untitled/src/com/company/Main.java
@@ -4,10 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.hacerComparacion(7, 5);
+        main.hacerComparacion(8, 10);
     }
 
     public void hacerComparacion(double x, double y) {
+       /* if(x==y){
+            System.out.println("los números son iguales");
+        }else{
+            double comparacion= (x<y)? y:x;
+            System.out.println("el número mayor es: "+ comparacion);
+        }*/
+
         if (x > y) {
             System.out.println(x + " es mayor que " + y);
         } else if (x == y) {
