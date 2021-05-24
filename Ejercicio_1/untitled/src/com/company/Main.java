c7d44f5ecc0c5cd20145728d601b89d72cf2f9f5
Lina <linazon94@gmail.comgit>
2021-05-23 19:47:30 -0500

    se elimina clase comparacion

--- Ejercicio_1/untitled/src/com/company/Main.java
+++ Ejercicio_1/untitled/src/com/company/Main.java
@@ -3,8 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Comparacion comparacion = new Comparacion();
-        comparacion.hacerComparacion(7, 5);
+        Main main = new Main();
+        main.hacerComparacion(7, 5);
     }
 
+    public void hacerComparacion(double x, double y) {
+        if (x > y) {
+            System.out.println(x + " es mayor que " + y);
+        } else if (x == y) {
+            System.out.println("Los valores son iguales");
+        } else {
+            System.out.println(y + " es mayor que " + x);
+        }
+    }
 }
