562f28301458df38901fef5d378105471c39e58e
Lina <linazon94@gmail.comgit>
2021-05-23 21:14:48 -0500

    se factoriza un poco creando el metodo crearParesImpares para hacer el while

---
+++ Ejercicio_4/IVA/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+I
+src/com/company/Main.java,4\a\4a831b10439099d44774d0ccc387e808deffaeab
+M
+src/com/company/Producto.java,3\6\36daf29b71ebf596cddd1757a85cf9e963c8ffaa
--- Ejercicio_5/Par Impar/src/com/company/Main.java
+++ Ejercicio_5/Par Impar/src/com/company/Main.java
@@ -4,22 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    public static List pares = new ArrayList();
+    public static List impares = new ArrayList();
 
     public static void main(String[] args) {
-        List pares = new ArrayList();
-        List impares = new ArrayList();
-
-        int i= 1;
-
-	while (i <= 100){
-	    if (i % 2 != 0){
-            impares.add(i);
-        }else{
-	        pares.add(i);
-        }
-	    i++;
-    }
+    Main main = new Main();
+    main.crearParesImpares();
 	System.out.println("Numeros pares: \n" +pares);
 	System.out.println("Numeros impares: \n" +impares);
+    }
+    public void crearParesImpares(){
+        int i = 1;
+        while (i <= 100){
+            if (i % 2 == 0) {
+                pares.add(i);
+            } else {
+                impares.add(i);
+            }
+            i++;
+        }
     }
 }
